6a8e0da0a3d1e8563d340c530c2f3fd1c370044d
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-05 07:35:46 +0900

    feat: homeViewModel에 getAddressByLatLngAndSearch 메소드 추가

--- lib/ui/home/home_view_model.dart
+++ lib/ui/home/home_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_search_app/core/geolocator_helper.dart';
 import 'package:flutter_search_app/data/model/location.dart';
 import 'package:flutter_search_app/data/repository/location_repository.dart';
+import 'package:flutter_search_app/data/repository/vworld_repository.dart';
 
 class HomeState {
   HomeState({required this.locations});
@@ -18,6 +20,28 @@ class HomeViewModel extends Notifier<HomeState> {
     LocationRepository locationRepository = LocationRepository();
 
     state = HomeState(locations: await locationRepository.search(query));
+  }
+
+  Future<List> getAddressByLatLngAndSearch() async {
+    // geoLocator로 위도, 경도 가져오기
+    final position = await GeolocatorHelper.getPositon();
+    dynamic address;
+    if (position != null) {
+      // vWorld API로 주소 가져오기
+      VworldRepository vworldRepository = VworldRepository();
+
+      address = await vworldRepository.findByLatLng(
+          lat: position.latitude, lng: position.longitude);
+    }
+
+    // Naver API에 받아온 주소를 보내 검색한 후 상태 업데이트
+    LocationRepository locationRepository = LocationRepository();
+    if (address.isNotEmpty) {
+      state = HomeState(locations: await locationRepository.search(address[0]));
+    }
+    // 검색창 업데이트를 추가하기 위해 주소값을 리턴해주었습니다.
+    // 빈 배열 리턴 여부로 현재 위치를 못찾는 상황 분기하기 위해 address[0]이 아닌 address를 리턴했습니다.
+    return address;
   }
 }
 
