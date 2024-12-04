b6f8e7cc5c347ac40d3c82ee3d17321a8ce4e301
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-05 02:19:22 +0900

    feat: homeViewModel 정의 및 테스트 구현

---
+++ test/home_view_model_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_search_app/ui/home/home_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+ProviderContainer createContainer() {
+  final container = ProviderContainer();
+  addTearDown(container.dispose);
+  return container;
+}
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  test(
+    'HomeViewModel test',
+    () async {
+      final container = createContainer();
+      HomeState homeState = container.read(homeViewModelProvider);
+
+      expect(homeState.locations, null);
+
+      await container.read(homeViewModelProvider.notifier).search('별내동');
+
+      HomeState homeStateAfterSearch = container.read(homeViewModelProvider);
+      expect(homeStateAfterSearch.locations!.isNotEmpty, true);
+      expect(homeStateAfterSearch.locations![0].title, "별내행정복지센터");
+    },
+  );
+}
