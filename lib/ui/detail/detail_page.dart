cb9ab8fb5d0bbbff9fcbb563382b9c0684ba1c3b
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-05 05:24:34 +0900

    feat: DetailPage 추가 및 homeResultView와 연결

---
+++ lib/ui/detail/detail_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key, required this.link, required this.title});
+
+  final String title;
+  final String link;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Html(
+          data: title,
+          style: {
+            // <b> 태그 일반적인 텍스트로 변환
+            "*": Style(
+              fontWeight: FontWeight.normal,
+              textAlign: TextAlign.center,
+            )
+          },
+        ),
+        actions: const [
+          SizedBox(
+              width: kToolbarHeight), // 정렬 맞추기 위해 leading 사이즈와 같은 sizedBox 추가
+        ],
+      ),
+      body: InAppWebView(
+        initialSettings: InAppWebViewSettings(
+          mediaPlaybackRequiresUserGesture: true,
+          javaScriptEnabled: true,
+          userAgent:
+              "Mozilla/5.0 (Linux; Android 10; Mobile; rv:89.0) Gecko/89.0 Firefox/89.0", // 모바일 페이지로 실행하도록 userAgent 설정
+        ),
+        initialUrlRequest: URLRequest(
+          url: WebUri(link),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/home/widgets/home_result_view.dart
+++ lib/ui/home/widgets/home_result_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_search_app/ui/detail/detail_page.dart';
 import 'package:flutter_search_app/ui/home/home_view_model.dart';
 
 class HomeResultView extends ConsumerWidget {
@@ -13,47 +14,91 @@ class HomeResultView extends ConsumerWidget {
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: locations == null || locations.isEmpty
+      child: locations == null
           ? Container(
               width: double.infinity,
               alignment: Alignment.center,
-              child: const Text("검색결과가 없습니다."))
-          : ListView.builder(
-              itemCount: locations.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      alignment: Alignment.centerLeft,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Html(data: locations[index].title, style: {
-                            "b": Style(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.greenAccent,
+              child: const Text("검색어를 입력해주세요."))
+          : locations.isEmpty
+              ? Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: const Text("검색결과가 없습니다."))
+              : ListView.builder(
+                  itemCount: locations.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if (locations[index].link.length >= 5 &&
+                                locations[index].link.startsWith("https")) {
+                              Navigator.push(context, MaterialPageRoute(
+                                builder: (context) {
+                                  return DetailPage(
+                                    title: locations[index].title,
+                                    link: locations[index].link,
+                                  );
+                                },
+                              ));
+                            }
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  spreadRadius: 2,
+                                  blurRadius: 10,
+                                ),
+                              ],
                             ),
-                            "*": Style(
-                                fontSize: FontSize(18),
-                                fontWeight: FontWeight.bold,
-                                margin: Margins.all(0))
-                          }),
-                          Text(locations[index].category),
-                          Text(locations[index].roadAddress),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    )
-                  ],
-                );
-              }),
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                locations[index].link.length >= 5 &&
+                                        locations[index]
+                                            .link
+                                            .startsWith("https")
+                                    // http는 접속 안되도록 했습니다.
+                                    ? const Column(
+                                        children: [
+                                          Icon(
+                                            Icons.language,
+                                            color: Colors.blue,
+                                          ),
+                                          SizedBox(
+                                            height: 4,
+                                          )
+                                        ],
+                                      )
+                                    : const SizedBox.shrink(),
+                                Html(data: locations[index].title, style: {
+                                  "b": Style(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue,
+                                  ),
+                                  "*": Style(
+                                      fontSize: FontSize(18),
+                                      fontWeight: FontWeight.bold,
+                                      margin: Margins.all(0))
+                                }),
+                                Text(locations[index].category),
+                                Text(locations[index].roadAddress),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        )
+                      ],
+                    );
+                  }),
     );
   }
 }
