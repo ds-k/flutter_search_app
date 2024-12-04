23931c7a852e25012d9d4e978320b4db250e0ccb
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-05 01:51:38 +0900

    feat: 연습용 .env 적용 (과제이기 때문에 .gitignore에 추가안함)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_search_app/ui/home/home.dart';
 
-void main() {
+void main() async {
+  // * 연습 목적으로 env를 추가했습니다. 과제 제출이기 때문에 .gitignore에 .env를 추가하지 않았습니다.
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
